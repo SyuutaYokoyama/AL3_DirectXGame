d67fe4cc2eacbff3c13f1f2859321c1c1a2fdddf
SyuutaYokoyama <k021g1440@g.neec.ac.jp>
2022-04-13 11:10:02 +0900

    変更

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -17,14 +17,14 @@ void GameScene::Initialize() {
 	sprite_ = Sprite::Create(textureHandle_, {100, 50});
 	textureHandle_ = TextureManager::Load("mario.jpg");
 	model_ = Model::Create();
-	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
-	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
-	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+	//worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+	//worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
+	//worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
 	worldTransform_.Initialize();
 	viewProjection_.Initialize();
 	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
-	//audio_->PlayWave(soundDataHandle_);
-	//voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+	audio_->PlayWave(soundDataHandle_);
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
 }
 
 void GameScene::Update() {
@@ -40,18 +40,18 @@ void GameScene::Update() {
 	// std::string strDebug = std::string("transration:4.0") + std::to_string(translation_);
 	// std::string strDebug2 = std::string("rotation:") + std::to_string(rotation_);
 	// std::string strDebug3 = std::string("transration:") + std::to_string(scale_);
-	debugText_->SetPos(50, 70);
-	debugText_->Printf(
-	  "translation:(%f,%f,%f)", worldTransform_.translation_.x, worldTransform_.translation_.y,
-	  worldTransform_.translation_.z);
-	debugText_->SetPos(50, 90);
-	debugText_->Printf(
-	  "rotation:(%f,%f,%f)", worldTransform_.rotation_.x, worldTransform_.rotation_.y,
-	  worldTransform_.rotation_.z);
-	debugText_->SetPos(50, 110);
-	debugText_->Printf(
-	  "scale:(%f,%f,%f)", worldTransform_.scale_.x, worldTransform_.scale_.y,
-	  worldTransform_.scale_.z);
+	//debugText_->SetPos(50, 70);
+	//debugText_->Printf(
+	//  "translation:(%f,%f,%f)", worldTransform_.translation_.x, worldTransform_.translation_.y,
+	//  worldTransform_.translation_.z);
+	//debugText_->SetPos(50, 90);
+	//debugText_->Printf(
+	//  "rotation:(%f,%f,%f)", worldTransform_.rotation_.x, worldTransform_.rotation_.y,
+	//  worldTransform_.rotation_.z);
+	//debugText_->SetPos(50, 110);
+	//debugText_->Printf(
+	// "scale:(%f,%f,%f)", worldTransform_.scale_.x, worldTransform_.scale_.y,
+	//  worldTransform_.scale_.z);
 
 	// debugText_->Print(strDebug2, 50, 70, 1.0f);
 	// debugText_->Print(strDebug3, 50, 90, 1.0f);
@@ -95,7 +95,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	// sprite_->Draw();
+	 sprite_->Draw();
 	//   デバッグテキストの描画
 	debugText_->DrawAll(commandList);
 	//
