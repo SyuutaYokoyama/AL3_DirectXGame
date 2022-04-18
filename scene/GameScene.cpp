df21a96f28497689b73c89e32dd0c5010a19365c
SyuutaYokoyama <k021g1440@g.neec.ac.jp>
2022-04-19 01:43:19 +0900

    変更

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -10,25 +10,28 @@ GameScene::GameScene() {}
 GameScene::~GameScene() { delete model_; }
 
 void GameScene::Initialize() {
+	std::random_device seed_gen;
+	std::mt19937_64 engine(seed_gen());
+	std::uniform_real_distribution<float> rotDist(0.0f, XM_2PI);
+	std::uniform_real_distribution<float> posDist(-10.0f, 10.0f);
+	worldTransform_->translation_ = {0.0f, 10.0f, 0.0f};
+	viewProjection_.target = {10, 0, 0};
+	viewProjection_.Initialize();
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
 	textureHandle_ = TextureManager::Load("mario.jpg");
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	sprite_ = Sprite::Create(textureHandle_, {100, 100});
 	textureHandle_ = TextureManager::Load("mario.jpg");
 	model_ = Model::Create();
-	std::random_device seed_gen;
-	std::mt19937_64 engine(seed_gen());
-	std::uniform_real_distribution<float> rotDist(0.0f, XM_2PI);
-	std::uniform_real_distribution<float> posDist(-10.0f, 10.0f);
 	for (size_t i = 0; i < _countof(worldTransform_); i++) {
 		worldTransform_[i].scale_ = {1.0f, 1.0f, 1.0f};
 		worldTransform_[i].rotation_ = {rotDist(engine), rotDist(engine), rotDist(engine)};
-		worldTransform_[i].translation_ = {rotDist(engine), rotDist(engine), rotDist(engine)};
+		worldTransform_[i].translation_ = {posDist(engine), posDist(engine), posDist(engine)};
 		worldTransform_[i].Initialize();
 	}
-	viewProjection_.Initialize();
+	
 	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
 	//audio_->PlayWave(soundDataHandle_);
 	//voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
@@ -61,9 +64,36 @@ void GameScene::Update() {
 	 //"scale:(%f,%f,%f)", worldTransform_.scale_.x, worldTransform_.scale_.y,
 	  //worldTransform_.scale_.z);
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 	// debugText_->Print(strDebug2, 50, 70, 1.0f);
 	// debugText_->Print(strDebug3, 50, 90, 1.0f);
+	XMFLOAT3 move = {0, 0, 0};
+	const float kEyeSpeed = 0.2f;
+	const float kTargetSpeed = 0.2f;
+	if (input_->PushKey(DIK_W)) {
+		move = {0, 0, kEyeSpeed};
+	} else if (input_->PushKey(DIK_S)) {
+		move = {0, 0, -kEyeSpeed};
+	}
+	if (input_->PushKey(DIK_LEFT)) {
+		move = {-kTargetSpeed, 0, 0};
+	} else if (input_->PushKey(DIK_RIGHT)) {
+		move = {kTargetSpeed, 0, 0};
+	}
+	viewProjection_.eye.x += move.x;
+	viewProjection_.eye.y += move.y;
+    viewProjection_.eye.z += move.z;
+	viewProjection_.target.x += move.x;
+	viewProjection_.target.y += move.y;
+	viewProjection_.target.z += move.z;
+
+	viewProjection_.UpdateMatrix();
+
+	debugText_->SetPos(50, 50);
+	debugText_->Printf(
+	  "eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
+	debugText_->SetPos(50, 70);
+	debugText_->Printf(
+	  "target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y, viewProjection_.target.z);
 }
 
 void GameScene::Draw() {
@@ -106,7 +136,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	// sprite_->Draw();
+	//sprite_->Draw();
 	//   デバッグテキストの描画
 	debugText_->DrawAll(commandList);
 	//
